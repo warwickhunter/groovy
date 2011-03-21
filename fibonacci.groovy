c72cc22adbf93f9975d4dfc873527585b0c6c356
Warwick Hunter <hunter@foodfone.com.au>
2011-03-21 21:32:27 +1000

    fibonacci

---
+++ fibonacci.groovy
@@ -0,0 +1,14 @@
+// Fibonacci sequence from the book Groovy in Action 2011-03-21
+current = 1 
+next = 1
+count = 0
+20.times {
+    printf "%8d ", current
+    newCurrent = next
+    next = next + current
+    current = newCurrent
+    if (++count % 10 == 0) {
+        println ''
+    } 
+}
+println ''
