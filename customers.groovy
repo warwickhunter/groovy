15fdbd6f04c9bc675521eb09b88ebf5495f14e21
Warwick Hunter <w.hunter@computer.org>
2011-03-22 20:43:53 +1000

    a little more messing about

--- customers.groovy
+++ customers.groovy
@@ -1,8 +1,10 @@
 #!/usr/bin/env groovy
 def customers = new XmlSlurper().parse(new File('customers.xml'))
+println "Corporate customers"
 for (customer in customers.corporate.customer) {
-    println "${customer.@name} works for ${customer.@company}"
+    println "    ${customer.@name} works for ${customer.@company}"
 }
+println "Consumers"
 for (customer in customers.consumer.customer) {
-    println "${customer.@name} is a consumer"
+    printf '    %s%n', customer.@name
 }
