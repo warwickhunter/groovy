0aa1524a070e8aa7da9b38b60ed09c8405dd19e3
Warwick Hunter <w.hunter@computer.org>
2011-04-03 09:14:11 +1000

    gpath code from chapter 7

---
+++ gpath.groovy
@@ -0,0 +1,11 @@
+/**
+ * Messing about with GPath
+ * 
+ * @author Warwick Hunter
+ * @since  2011-04-03
+ */
+this
+this.class
+this.class.methods
+this.class.methods.name
+this.class.methods.name.grep(~/get.*/).sort()
