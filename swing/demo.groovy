44694734384f30284867e5fc9a67506e03f40798
Warwick Hunter <w.hunter@computer.org>
2011-04-10 12:25:20 +1000

    Swing example

--- swing/demo.groovy
+++ swing/demo.groovy
@@ -6,20 +6,27 @@
  * @since  2011-04-10
  */
 import groovy.swing.SwingBuilder
+import javax.swing.*
+import java.awt.event.*
 
 swing = new SwingBuilder()
 frame = swing.frame(title:'Demo') {
     menuBar {
         menu('File') {
-            menuItem 'New'
-            menuItem 'Open'
+            menuItem(label:'New', actionPerformed: { println "New file" })
+            menuItem(label:'Open', actionPerformed: { println "Open file" })
         }
     }
     panel {
         label 'Label 1'
-        slider()
-        comboBox(items:['one','two','three'])
+        slider(stateChanged:{ println "Slide $it.source.value" })
+        comboBox(items:['one','two','three'], itemStateChanged:{ event ->
+            if (event.stateChange == ItemEvent.SELECTED) {
+                println "$event.source.selectedItem"
+            } 
+        })
     }
 }
 frame.pack()
+frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 frame.show()
