3b0e78257bd5f0b4c7ca721400c651edf67c80d3
Warwick Hunter <w.hunter@computer.org>
2011-04-10 10:42:36 +1000

    ant builder example

---
+++ ant_builder.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+/**
+ * Ant builder example from "Groovy in Action"
+ *
+ * @author Warwick Hunter
+ * @since  2011-04-10
+ */
+
+// Add the ant jar to the classpath at runtime
+groovyHome = System.getenv('GROOVY_HOME')
+new File("$groovyHome/lib").eachFileMatch(~/ant.*\.jar/, { 
+    this.class.classLoader.rootLoader.addURL(new URL("file:$it"))
+})
+
+BUILD_DIR = "build"
+SRC_DIR = "."
+
+def ant = new AntBuilder()
+ant.delete(dir:BUILD_DIR)
+ant.mkdir(dir:BUILD_DIR)
+ant.sequential {
+    taskdef name:"groovyc", classname:"org.codehaus.groovy.ant.Groovyc"
+    groovyc srcdir:SRC_DIR, destdir:BUILD_DIR, {
+        javac source:"1.6", target:"1.6", debug:"on"
+    }
+    jar destfile:"$BUILD_DIR/mygroovy.jar", {
+        fileset dir:BUILD_DIR, {
+            include name:"*.class"
+        }
+    }
+}
