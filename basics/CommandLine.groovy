cb58b06e1b8e35f5a13925add852d5cc7f85352f
Warwick Hunter <w.hunter@computer.org>
2011-08-27 16:21:24 +1000

    Command line options handling

---
+++ basics/CommandLine.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/env groovy
+package basics
+/**
+ * Groovy sample from "Groovy in Action" book. Using CliBuilder to handle command line arguments
+ * 
+ * @author Warwick Hunter
+ * @since  2011-08-27
+ */
+
+def cli = new CliBuilder()
+cli.h(longOpt:'help', 'Usage information')
+cli.f(longOpt:'file', 'Input file', required:true, args:1, type:String)
+
+def options = cli.parse(args)
+if (!options) {
+    return
+}
+if (options.h) {
+    cli.usage()
+}
+println "Do something to file $options.f"
+
+
