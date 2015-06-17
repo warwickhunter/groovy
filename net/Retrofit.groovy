5dfe1916591616d6771a980b230abdfcab16c112
Warwick Hunter <w.hunter@computer.org>
2015-06-18 08:01:21 +1000

    Retrofit example

---
+++ net/Retrofit.groovy
@@ -0,0 +1,12 @@
+/**
+ * Experiment with the Retrofit JSON REST library
+ * 
+ * http://square.github.io/retrofit/
+ *
+ * @author Warwick Hunter
+ * @since  2015-06-18
+ */
+
+// Use Grape to handle the downloading of required jars, nice!
+@Grab(group='com.squareup.retrofit', module='retrofit', version='1.9.0')
+
