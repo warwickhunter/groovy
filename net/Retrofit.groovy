d6e76a1016d133b2d2f65b12b8cd158230b79615
Warwick Hunter <w.hunter@computer.org>
2015-06-18 09:06:15 +1000

    finish retrofit example

--- net/Retrofit.groovy
+++ net/Retrofit.groovy
@@ -10,3 +10,20 @@
 // Use Grape to handle the downloading of required jars, nice!
 @Grab(group='com.squareup.retrofit', module='retrofit', version='1.9.0')
 
+import retrofit.*
+import retrofit.http.*
+
+interface GitHubService {
+  @GET("/users/{user}/repos")
+  List<Object> listRepos(@Path("user") String user);
+}
+
+RestAdapter restAdapter = new RestAdapter.Builder()
+    .setEndpoint("https://api.github.com")
+    .build();
+
+GitHubService service = restAdapter.create(GitHubService.class);
+
+println service.listRepos("octocat");
+
+return 0
