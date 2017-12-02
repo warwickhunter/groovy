f87a018c2a6960604332d725c8ad584142d630a9
Warwick Hunter <w.hunter@computer.org>
2017-12-03 07:16:52 +1000

    Explain the multiply by 1000

--- basics/PrintTimes.groovy
+++ basics/PrintTimes.groovy
@@ -5,7 +5,7 @@ ArrayList<Long> times = [1511884389L,1511874638L,1511896613L,1511870978L]
 
 Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"))
 times.each { time ->
-    Date d = new Date(time * 1000)
+    Date d = new Date(time * 1000) // Java expects milliseconds since the epoch, UNIX uses seconds
     cal.setTime(d)
     printf '%1$tF %1$tT %1$tZ %n', cal
 }
