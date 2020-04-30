ada0949cb27f110bfbb848c03e356fd7920d4f30
Warwick Hunter <w.hunter@computer.org>
2020-04-30 13:22:30 +1000

    helper

---
+++ basics/generateContactsCsv.groovy
@@ -0,0 +1,3 @@
+for (int i = 2; i < 1000; ++i) {
+    printf("Contact %d,Contact,,%d,,,,,,,,,,,,,,,,,,,,,,,,,,,contact.%d@sudomail.com,,,,,,,,Mobile,+61  4 1234 %04d,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,%n", i, i, i, i)
+}
